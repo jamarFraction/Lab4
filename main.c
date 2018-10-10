76235dcc20631e90d8ca622c36f127057c112420
Jamar Fraction <jamar.fraction@wsu.edu>
2018-10-10 11:23:31 -0700

    tokenizing for cp function

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>      // for strcpy(), strcmp(), etc.
 #include <libgen.h>      // for basename(), dirname()
 #include <fcntl.h>       // for open(), close(), read(), write()
+#include <stdbool.h>
 
 // for stat syscalls
 #include <sys/stat.h>
@@ -12,21 +13,72 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+char *userCommand[16];    //array to hold the strings of a command
+int myargc = 0;
 
+void tokenize(char source[]);
+int myrcp(char *f1, char *f2);
 
 int main(int argc, char *argv[]){
 
     printf("Hey, friends.\n");
 
-    if (argc < 3)
-        //print usage and exit;
-    return myrcp(argv[1], argv[2]);
+    char lineCpy[128];
+    char line[128];     //raw user line
 
-    return 0;
+    //Prompt user for a command
+    printf("Command: ");
+    fflush(stdout);
+
+    fgets(line, 128, stdin);
+
+    //chop the tail off the line, set to null
+    line[strlen(line) - 1] = 0;
+
+    strcpy(lineCpy, line);
+
+    //results stored in global userCommand array
+    tokenize(lineCpy);
+
+    //check for form fitting command
+    if (myargc < 3)
+    {
+        //print usage and exit?
+        //whatever that means.. will return
+        return 0;
+    }
+
+    //Let's get to it!
+    return myrcp(userCommand[1], userCommand[2]);
+}
+
+void tokenize(char source[])
+{
+
+    //temp string holder of strtok vals
+    char *next = strtok(source, " ");
+
+    int i = 0;
+
+    //grab all space-separated user input
+    //put in arg array
+    while (next != 0)
+    {
+
+        userCommand[i] = next;
+
+        next = strtok(NULL, " ");
+
+        i++;
+        myargc += 1;
+    }
 }
 
 int myrcp(char *f1, char *f2)
 {
+    
+
+    printf("oh, hey there.\n");
     // {
     //    1. stat f1;   if f1 does not exist ==> exit.
     //                  f1 exists: reject if f1 is not REG or LNK file
